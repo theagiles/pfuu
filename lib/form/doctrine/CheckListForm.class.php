5c30f7242457a060b63d6d5f788621cf081f72fc
stiven castro <stivencastroarias2@gmail.com>
2019-10-03 12:26:49 -0500

    renderizado de la plantiulla form

--- lib/form/doctrine/CheckListForm.class.php
+++ lib/form/doctrine/CheckListForm.class.php
@@ -17,13 +17,13 @@ class CheckListForm extends BaseCheckListForm
     $this->widgetSchema['reference']        = new sfWidgetFormInputText(array(), array( 'class' => 'form-control'));
     $this->widgetSchema['template_id']       = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Template'), 'add_empty' => false), array( 'class' => 'form-control'));
     $this->widgetSchema['responsible_id']    = new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Responsible'), 'add_empty' => false), array( 'class' => 'form-control'));
-    $this->widgetSchema['original_threshold'] = new sfWidgetFormInputText(array(), array( 'class' => 'form-control'));
+   
 
       $this->widgetSchema['status'] = new sfWidgetFormChoice(
           array(
               'choices' => array(1 => 'Activo', 0 => 'Inactivo'),
           ), array( 'class' => 'form-control'));
 
-    $this->useFields(['name','reference', 'observations', 'template_id', 'responsible_id', 'original_threshold', 'status']);
+    $this->useFields(['name','reference', 'observations', 'template_id', 'responsible_id', 'status']);
   }
 }
--- lib/form/doctrine/TemplateForm.class.php
+++ lib/form/doctrine/TemplateForm.class.php
@@ -12,7 +12,11 @@ class TemplateForm extends BaseTemplateForm
 {
   public function configure()
   {
+    //Esta linea ayuda a pintar mejor  el  recuadro del fomulario con las propiedas de form-control
     $this->widgetSchema['name']=new  sfWidgetFormInputText(array(),array("class"=>"form-control"));
-   $this->useFields(['name','description','prefix']); 
+    $this->widgetSchema['description'] = new  sfWidgetFormTextarea(array(),array("class"=>"form-control"));
+    $this->widgetSchema['prefix'] = new sfWidgetFormInputText(array(),array("class"=>"form-control"));
+  
+    $this->useFields(['name','description','prefix']); 
   }
 }
